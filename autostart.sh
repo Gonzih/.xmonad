591e5b45a1363aa2e4f642d21cffb971d6ce97e3
Max Gonzih <gonzih@gmail.com>
2012-10-07 20:52:22 +0300

    add autostart script

---
+++ autostart.sh
@@ -0,0 +1,3 @@
+xsetroot -cursor_name left_ptr
+xsetroot -solid black
+trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --transparent true --tint 0x191970 --height 12 &
