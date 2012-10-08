e430eedf84b375aa9cf240f2ac0bd333da2b020c
Max Gonzih <gonzih@gmail.com>
2012-10-08 18:18:35 +0300

    use a for rename workspace, o for ad workspace, e for rm workspace

--- xmonad.hs
+++ xmonad.hs
@@ -155,10 +155,10 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm              , xK_q     ), spawn "xmonad --recompile; xmonad --restart")
 
     -- Add workspace with prompt
-    , ((modm              , xK_a     ), addWorkspacePrompt myPromptConfig)
+    , ((modm              , xK_o     ), addWorkspacePrompt myPromptConfig)
 
     -- Rename workspace
-    , ((modm              , xK_o     ), renameWorkspace myPromptConfig)
+    , ((modm              , xK_a     ), renameWorkspace myPromptConfig)
 
     -- Remove workspace
     , ((modm              , xK_e     ), removeEmptyWorkspace)
