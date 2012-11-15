f95a9db9bf9254fcc0b8db23598ab4e6e484f3d8
Max Gonzih <gonzih@gmail.com>
2012-11-15 14:50:38 +0300

    increase trayer height

--- autostart.sh
+++ autostart.sh
@@ -2,4 +2,4 @@ xsetroot -cursor_name left_ptr
 xsetroot -solid black
 trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true \
         --width 15 --transparent true --tint 0x000000 \
-        --heighttype pixel --height 22 &
+        --heighttype pixel --height 24 &
