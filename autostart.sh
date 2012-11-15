b676dca93616c4d0021eda216d942d17d937c4e4
Max Gonzih <gonzih@gmail.com>
2012-11-15 15:00:22 +0300

    use trayer-srg since it is better

--- autostart.sh
+++ autostart.sh
@@ -1,5 +1,5 @@
 xsetroot -cursor_name left_ptr
 xsetroot -solid black
 trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true \
-        --width 15 --transparent true --tint 0x000000 \
-        --heighttype pixel --height 24 &
+        --widthtype percent --width 15 --transparent true --tint 0x000000 \
+        --heighttype pixel --height 24 --monitor primary &
