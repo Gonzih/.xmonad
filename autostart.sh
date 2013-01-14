9aee2b270e4390155d4f24d739a0cf441a4f38b6
Max Gonzih <gonzih@gmail.com>
2013-01-14 10:22:45 +0300

    adjust trayer height

--- autostart.sh
+++ autostart.sh
@@ -2,5 +2,5 @@ xsetroot -cursor_name left_ptr
 xsetroot -solid black
 trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true \
         --widthtype percent --width 15 --transparent true --tint 0x000000 \
-        --heighttype pixel --height 24 --monitor primary &
+        --heighttype pixel --height 26 --monitor primary &
 xloadimage -onroot -fullscreen ~/Dropbox/Public/pics/wall/wallpaper-2496096.jpg&
