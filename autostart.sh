bb5bd4dd46b5e569415f6c7031059789f690880d
Max Gonzih <gonzih@gmail.com>
2013-01-14 10:23:20 +0300

    switch to quokka wallpaper

--- autostart.sh
+++ autostart.sh
@@ -3,4 +3,4 @@ xsetroot -solid black
 trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true \
         --widthtype percent --width 15 --transparent true --tint 0x000000 \
         --heighttype pixel --height 26 --monitor primary &
-xloadimage -onroot -fullscreen ~/Dropbox/Public/pics/wall/wallpaper-2496096.jpg&
+xloadimage -onroot -fullscreen ~/Dropbox/Public/pics/wall/quokka-wall.jpg
