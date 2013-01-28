f0b7acb1dc233bb3431b5db3bb5c807680c3cb66
Max Gonzih <gonzih@gmail.com>
2013-01-28 12:45:44 +0300

    replace on and off in mic state with + and -

--- mic-state.sh
+++ mic-state.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-amixer get Capture | grep -E '\b(on|off)\b' | sed 's/.*\(on\|off\).*/\1/'
+amixer get Capture | grep -E '\b(on|off)\b' | sed 's/.*\(on\|off\).*/\1/' | sed 's/on/<fc=#00ff00>+<\/fc>/' | sed 's/off/<fc=#ff0000>-<\/fc>/'
