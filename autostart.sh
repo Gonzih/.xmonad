f821c64b39d1d1867b5880a12465a0dd6e7879a3
Max Gonzih <gonzih@gmail.com>
2013-06-14 19:44:35 +0300

    Back to trayer-sng (OBS is sweet)

--- autostart.sh
+++ autostart.sh
@@ -1,8 +1,8 @@
 #xsetroot -cursor_name left_ptr
 #xsetroot -solid black
-#trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true \
-        #--widthtype percent --width 15 --transparent true --tint 0x000000 \
-        #--heighttype pixel --height 25 --monitor primary &
-sleep 2
-stalonetray --no-shrink --geometry 12x1+1630+0 --transparent false --background black --max-geometry 13x1  --icon-size 25 &
+trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true \
+       --widthtype percent --width 15 --transparent true --tint 0x000000 \
+       --heighttype pixel --height 25 --monitor primary &
+#stalonetray --no-shrink --geometry 12x1+1630+0 --transparent false \
+#            --background black --max-geometry 13x1  --icon-size 25 &
 #xloadimage -onroot -fullscreen ~/Dropbox/Public/pics/wall/quokka-wall.jpg
