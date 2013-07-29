15a6dc962a30e0676b74e2c49db8bfac1391cb3a
Max Gonzih <gonzih@gmail.com>
2013-07-29 12:38:05 +0300

    Add sh to screenshot scipts

--- scripts/area-screenshot.sh
+++ scripts/area-screenshot.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/sh
+
 scrot -b -s /tmp/area-screenshot.png
 geturl /tmp/area-screenshot.png
 notify-send -i ~/.xmonad/scripts/screenshot.png 'Uploaded screenshot' 'to filepicker.io'
--- scripts/full-screenshot.sh
+++ scripts/full-screenshot.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/sh
+
 scrot /tmp/full-screenshot.png
 geturl /tmp/full-screenshot.png
 notify-send -i ~/.xmonad/scripts/screenshot.png 'Uploaded screenshot' 'to filepicker.io'
