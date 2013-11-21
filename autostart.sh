7696b07624258395c79f4c4100fb542598bd34ba
Max Gonzih <gonzih@gmail.com>
2013-11-21 23:32:35 +0300

    Adjust xmobar for mate

--- autostart.sh
+++ autostart.sh
@@ -1,8 +1,9 @@
 #xsetroot -cursor_name left_ptr
 #xsetroot -solid black
-trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true \
-       --widthtype percent --width 15 --transparent true --tint 0x000000 \
-       --heighttype pixel --height 25 --monitor primary &
+#trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true \
+       #--widthtype percent --width 15 --transparent true --tint 0x000000 \
+       #--heighttype pixel --height 25 --monitor primary &
 #stalonetray --no-shrink --geometry 12x1+1630+0 --transparent false \
 #            --background black --max-geometry 13x1  --icon-size 25 &
 #xloadimage -onroot -fullscreen ~/Dropbox/Public/pics/wall/quokka-wall.jpg
+numlockx on&
