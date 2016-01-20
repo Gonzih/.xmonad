a7e1d53fa5bd0316b69d5b53c32ff91e6b9e86ab
Max Gonzih <gonzih@gmail.com>
2016-01-20 16:36:26 +0100

    3 screens!

--- autostart.sh
+++ autostart.sh
@@ -9,6 +9,6 @@
 numlockx on&
 wmname LG3D &
 # xfce4-session &
-# setxkbmap -option -option ctrl:nocaps -option grp:lctrl_toggle 'us(dvp),ru' &
+setxkbmap -option -option ctrl:nocaps -option grp:lctrl_toggle 'us(dvp),ru' &
 # xcompmgr -c &
 (sleep 1 && xfce4-panel --restart) &
