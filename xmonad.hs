03767d7dab44990662cf8c286e66d9fa73f4971a
Max Gonzih <gonzih@gmail.com>
2016-09-20 14:05:45 +0200

    Latest multiscreen setup fixes

--- xmonad.hs
+++ xmonad.hs
@@ -235,7 +235,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     -- mod-shift-{; , .}, Move client to screen 1, 2, or 3
     --
     [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
-        | (key, sc) <- zip [xK_period, xK_p, xK_semicolon, xK_comma] [0..]
+        | (key, sc) <- zip [xK_period, xK_comma, xK_semicolon] [0..]
         , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
     ++
