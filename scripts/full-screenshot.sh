5970f26dfb47468eeca7c66bb5736b0930c7e7b2
Max Gonzih <gonzih@gmail.com>
2017-03-22 11:44:27 +0100

    Use transfer

--- scripts/full-screenshot.sh
+++ scripts/full-screenshot.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env fish
 
 scrot /tmp/full-screenshot.png
-dropbox-upload /tmp/full-screenshot.png
+transfer-upload /tmp/full-screenshot.png
 notify-send -i ~/.xmonad/scripts/screenshot.png 'Uploaded screenshot' 'of desktop to dropbox'
