1e36e32f0e9da554d0d534f7cd4e9290d95f9d23
Max Gonzih <gonzih@gmail.com>
2020-03-28 14:52:26 -0400

    Remoev screenshot uploading

--- scripts/area-screenshot.sh
+++ scripts/area-screenshot.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env fish
 
 import /tmp/area-screenshot.png
-transfer-upload /tmp/area-screenshot.png
-notify-send -i ~/.xmonad/scripts/screenshot.png 'Uploaded screenshot' 'of area to dropbox'
+# transfer-upload /tmp/area-screenshot.png
+# notify-send -i ~/.xmonad/scripts/screenshot.png 'Uploaded screenshot' 'of area to dropbox'
